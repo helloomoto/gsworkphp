ec3470171a8bea4452a75bc42326f31f91aa281b
猪川元朗 <moto@chokawamotoakira-no-MacBook-Pro.local>
2017-06-13 20:13:33 +0900

    更新しました！

--- question/output_data.php
+++ question/output_data.php
@@ -1,31 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-<?php
-echo '<table border="1">\n';
-echo "<tr><th>name</th><th>email</th><th>b_type</th></tr>\n";
-$filename = "data/data.csv";
-$lines = file($filename);
-foreach($lines as $key => $line){
-    explode(",",$line);
-    echo "<tr><td>{$line[0]}</td><td>{$line[1]}</td><td>{$line[2]}</td><tr>\n";
-}
-?>
-<table border="1">
-    <tr>
-        <th>name</th>
-        <th>email</th>
-        <th>b_type</th>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-</table>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <table border="1">
+            <tr><th>"お名前"</th><th>"Email"</th><th>"血液型"</th></tr>
+        
+        <?php
+                $file = fopen("data/data.csv","r");
+                while(!feof($file)){
+                    $array = fgetcsv($file);
+                    echo("<tr><th>".$array[0]."</th><th>".$array[1]."</th><th>".$array[2]."</th></tr><br>");
+                }
+        ?>
+
+        </table>
+    </body>
 </html>
